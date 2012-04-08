d3f3d858e9ea0e5e4c9c53bbcf6eb2ee116f56c7
Max Ogden <mogden@gmail.com>
2012-04-08 15:40:06 -0400

    implement XHRStream class as an EventEmitter

--- streaming-xhr.js
+++ streaming-xhr.js
@@ -1,3 +1,5 @@
+var EventEmitter = require('events').EventEmitter
+
 document.addEventListener('DOMContentLoaded', function() {
 
   fetch('http://localhost:8001/octet-stream', '#octet-stream')
@@ -7,17 +9,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function fetch(url, outputSelector) {
   var count = document.querySelectorAll(outputSelector)[0]
-
   var xhr = new XMLHttpRequest()
   xhr.open("GET", url, true)
-
-  xhr.onreadystatechange = function() {
-    var data = xhr.responseText
+  var stream = new XHRStream(xhr)
+  
+  stream.on('data', function(data) {
     var span = document.createElement('span')
-    span.innerHTML = "readystate " + xhr.readyState + ": " + data.toString() + '<br>'
+    span.innerHTML = data + '<br>'
     count.appendChild(span)
-    return data
-  }
+  })
+  stream.on('end', function() { })
+}
+
+function XHRStream(xhr) {
+  var me = this
+  me.xhr = xhr
+  me.offset = 0
+ 
+  xhr.onreadystatechange = function () { me.handle() }
   
   xhr.send(null)
+}
+
+XHRStream.prototype = new EventEmitter
+
+XHRStream.prototype.handle = function () {
+  if (this.xhr.readyState === 3) this.write()
+  if (this.xhr.readyState === 4) this.emit('end')
+}
+
+XHRStream.prototype.write = function () {
+  if (this.xhr.responseText.length > this.offset) {
+    this.emit('data', this.xhr.responseText.slice(this.offset))
+    this.offset = this.xhr.responseText.length
+  }
 }
